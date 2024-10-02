4f05f6e843640806ea4ed9a37e6679e653305ab9
SHAHINUR <shahinuralam.cse@gmail.com>
2024-10-01 23:53:27 -0400

    Create YOLOv8 pretrained.py

---
+++ YOLOv8 pretrained.py
@@ -0,0 +1,43 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+# Load the YOLOv8 model
+model = YOLO('yolov8x.pt')  # 'n' for nano model, you can use 's', 'm', 'l', or 'x' for other sizes
+
+
+def process_video(video_path):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    while cap.isOpened():
+        # Read a frame from the video
+        success, frame = cap.read()
+
+        if success:
+            # Run YOLOv8 inference on the frame
+            results = model(frame)
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+            # Break the loop if 'q' is pressed
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            # Break the loop if the end of the video is reached
+            break
+
+    # Release the video capture object and close the display window
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+# Specify the path to your video file
+video_path = "input/video.mp4"
+
+# Process the video
+process_video(video_path)
